4e675c639dac0aba746428350c467a529e839325
Harsh Raj <95171067+Harshsaxena18@users.noreply.github.com>
2025-01-02 12:52:08 +0530

    Update user.js

--- src/data/user.js
+++ src/data/user.js
@@ -17,7 +17,7 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Frontrend Developer.",
+		title: "Hey, I'm a Frontrend Developer.",
 		description:
 			"I am a Frontend Developer with expertise in Web and Mobile Application. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
