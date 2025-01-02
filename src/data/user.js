24c3888b35f711634182e69016566fcbb57f7f2f
Harsh Raj <95171067+Harshsaxena18@users.noreply.github.com>
2025-01-02 12:47:33 +0530

    Update user.js

--- src/data/user.js
+++ src/data/user.js
@@ -17,9 +17,9 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Full-Stack Web and Mobile App Developer.",
+		title: "Frontrend Developer.",
 		description:
-			"I am a Full Stack Developer with expertise in Web and Mobile Application. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"I am a Frontend Developer with expertise in Web and Mobile Application. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
 
 	about: {
